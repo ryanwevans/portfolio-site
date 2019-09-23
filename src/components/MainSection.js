99ccecb8606c3638a8f65c132a001abb0b8f0c8b
Ryan <ryanwevans@icloud.com>
2019-09-22 21:27:42 -0700

    Add MainSection and main image

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Image from 'react-bootstrap/Image';
 
 const MainSection = () => {
     return (
-        <div>
-            <Image src={require('../images/main_image.jpg')} alt='Landscape' className='MainImage' />
+        <div className='MainBackgroundImage'>
+            <div class="col-md-12 text-center">
+                <h1 class="h1-reponsive white-text text-uppercase font-weight-light mb-4 pt-md-5 pt-5 fadeInDown" data-wow-delay="0.3s">Ryan Evans</h1>
+                <h5 class="text-uppercase mb-0 fadeInDown" data-wow-delay="0.4s">Full Stack Developer</h5>
+            </div>
         </div>
     );
 }
