fc1dd6747950de7b6218035f998fcf8a94ed903f
Ryan <ryanwevans@icloud.com>
2019-09-22 21:28:46 -0700

    Update Navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ import Nav from 'react-bootstrap/Nav';
 const NavBar = () => {
     return (
         <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">RyanEvns</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#features">Portfolio</Nav.Link>
+          <Nav.Link href="#pricing">Contact</Nav.Link>
         </Nav>
         {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
