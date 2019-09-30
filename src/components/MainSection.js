457fc6f96a6c688914ffea628dd4ee84d8c1d872
Ryan <ryanwevans@icloud.com>
2019-09-30 14:37:23 -0700

    Update main text position and styling

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -4,8 +4,8 @@ const MainSection = () => {
     return (
         <div className='MainBackgroundImage'>
             <div class="col-md-12 text-center main-section">
-                <h1 class="h1-reponsive white-text text-uppercase font-weight-light mb-4 pt-md-5 pt-5" data-wow-delay="0.3s">Ryan Evans</h1>
-                <h3 class="text-uppercase mb-0 fadeInDown main-section-h3" data-wow-delay="0.4s">Full Stack Developer</h3>
+                <h3 class="text-uppercase main-section-h3 font-weight-lighter pt-5" data-wow-delay="0.3s">Ryan Evans</h3>
+                <h4 class="text-uppercase main-section-h3 font-weight-lighter" data-wow-delay="0.4s">Full Stack Developer</h4>
             </div>
         </div>
     );
