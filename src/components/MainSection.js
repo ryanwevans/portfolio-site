e13f16c71f34da126d4fa40ed6d8fc4ee0d2b92e
Ryan <ryanwevans@icloud.com>
2019-09-30 16:57:53 -0700

    Update Bio text; Add nav to sections

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainSection = () => {
     return (
-        <div className='MainBackgroundImage'>
+        <div className='MainBackgroundImage' id='home'>
             <div class="col-md-12 text-center main-section">
                 <h3 class="text-uppercase main-section-h3 font-weight-lighter pt-5" data-wow-delay="0.3s">Ryan Evans</h3>
                 <h4 class="text-uppercase main-section-h3 font-weight-lighter" data-wow-delay="0.4s">Full Stack Developer</h4>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { Link, animateScroll as scroll } from "react-scroll";
 
 const NavBar = () => {
     return (
-        <Navbar className='font-weight-light' bg="dark" variant="dark" fixed="top">
+        <Navbar className='font-weight-light' bg="dark" variant="dark" fixed='top'>
         <Navbar.Brand href="#home">RyanEvns</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#bio">Bio</Nav.Link>
-          <Link
-              activeClass="active"
-              to="#portfolio"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration= {500}
-          >Portfolio</Link>
+          <Nav.Link href="#portfolio">Portfolio</Nav.Link>
           <Nav.Link href="#contact">Contact</Nav.Link>
         </Nav>
       </Navbar>
