036e043537454ccf33d6eb85bf2871a5b24dbc2c
Ryan <ryanwevans@icloud.com>
2019-09-30 20:10:47 -0700

    Begin adding contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 
 const Contact = () => {
     return(
         <div className='contact-section' id='contact'>
             <div className='contact-container font-weight-light'>
                 <h3 className='section-heading font-weight-lighter text-uppercase'>Contact</h3>
+                <Container className='contact-form-container'>
+                    <Row>
+                        <Col className='contact-form'>
+                        <Form>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridEmail">
+                                <Form.Label>First Name</Form.Label>
+                                <Form.Control type="email" placeholder="First name" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridPassword">
+                                <Form.Label>Last Name</Form.Label>
+                                <Form.Control type="password" placeholder="Last name" />
+                                </Form.Group>
+                            </Form.Row>
+
+                            <Form.Group controlId="formGridAddress1">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control placeholder="name@email.com" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formGridAddress2">
+                                <Form.Label>Message</Form.Label>
+                                <Form.Control as="textarea" rows="5" placeholder="Message..." />
+                            </Form.Group>
+
+                            <Form.Group id="formGridCheckbox">
+                                <Form.Check type="checkbox" label="Something to Check" />
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">
+                                Send
+                            </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import * as Scroll from 'react-scroll';
-import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+// import * as Scroll from 'react-scroll';
+import { Link } from 'react-scroll';
+// import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 const NavBar = () => {
     return (
       <Navbar className='font-weight-light navigation' bg="dark" variant="dark" fixed='top'>
         <Navbar.Brand>
-          <Link className='nav-brand' to="home" spy={true} smooth={true} duration={500}>RyanEvns</Link>
+          <Nav.Link>
+            <Link className='nav-brand-text' to="home" spy={true} smooth={true} duration={500}>RyanEvns</Link>
+          </Nav.Link>
         </Navbar.Brand>
-        <Nav className="mr-auto nav-links-container">
+        <Nav className="mr-auto nav-links-container" justify>
           <Nav.Link>
           <Link className='nav-link-texts' to="home" spy={true} smooth={true} duration={800}>Home</Link>
           </Nav.Link>
@@ -18,10 +21,10 @@ const NavBar = () => {
           <Link className='nav-link-texts' to="bio" spy={true} smooth={true} duration={800} offset={-50}>Bio</Link>
           </Nav.Link>
           <Nav.Link>
-          <Link className='nav-link-texts' to="portfolio" spy={true} smooth={true} duration={800}>Portfolio</Link>
+          <Link className='nav-link-texts' to="portfolio" spy={true} smooth={true} duration={800} offset={-50}>Portfolio</Link>
           </Nav.Link>
           <Nav.Link>
-          <Link className='nav-link-texts' to="contact" spy={true} smooth={true} duration={800}>Contact</Link>
+          <Link className='nav-link-texts' to="contact" spy={true} smooth={true} duration={800} offset={-50}>Contact</Link>
           </Nav.Link>
         </Nav>
       </Navbar>
